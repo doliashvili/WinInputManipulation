da71919b9053e02d6e206f6fea4df2317548976b
Levan Doliashvili <levan.doliashvili@365emoney.onmicrosoft.com>
2022-06-16 12:13:07 +0400

    add package project

---
+++ InputSimulatorCorePackage/WindowsInput.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9e1ff335-ed74-4135-92cf-5e4cf1d38344")]
